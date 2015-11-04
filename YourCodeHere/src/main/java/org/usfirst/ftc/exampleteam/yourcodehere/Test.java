9d8300434353bcc61c2dd58c540f65a5aab38516
Andrew03 <andrewhoang03@gmail.com>
2015-11-03 23:43:35 -0800

    updated motor values and prototyped some threads

--- YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/Test.java
+++ YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/Test.java
@@ -50,29 +50,29 @@ public class Test extends SynchronousOpMode {
 
 
     // all of the important constants
-    final float STOP                   = 0.0f,
-                MAX_POWER              = 1.0f;
+    final double    STOP                   = 0.0d,
+                    MAX_POWER              = 1.0d;
 
     // all of the constant motor powers
-    final float PICKUP_POWER = 1.0f,
-                LIFT_POWER = 1.0f;
+    final double    PICKUP_POWER    = 1.0d,
+                    LIFT_POWER      = 1.0d;
 
     // all of the starting servo positions
-    final float S_CLIMBERS_START_POS_R = 0.0f,
-                S_CLIMBERS_START_POS_L = 0.0f,
-                S_LIFT_START_POS_R     = 0.0f,
-                S_LIFT_START_POS_L     = 0.0f,
-                S_BASKET_START_POS_R   = 0.0f,
-                S_BASKET_START_POS_L   = 0.0f,
-                S_PICKUP_START_POS_FR  = 0.0f,
-                S_PICKUP_START_POS_FL  = 0.0f,
-                S_PICKUP_START_POS_SR  = 0.0f,
-                S_PICKUP_START_POS_SL  = 0.0f,
-                S_HITCH_START_POS_R    = 0.0f,
-                S_HITCH_START_POS_L    = 0.0f;
+    final double    S_CLIMBERS_START_POS_R = Servo.MIN_POSITION,
+                    S_CLIMBERS_START_POS_L = Servo.MIN_POSITION,
+                    S_LIFT_START_POS_R     = Servo.MIN_POSITION,
+                    S_LIFT_START_POS_L     = Servo.MIN_POSITION,
+                    S_BASKET_START_POS_R   = Servo.MIN_POSITION,
+                    S_BASKET_START_POS_L   = Servo.MIN_POSITION,
+                    S_PICKUP_START_POS_FR  = Servo.MIN_POSITION,
+                    S_PICKUP_START_POS_FL  = Servo.MIN_POSITION,
+                    S_PICKUP_START_POS_SR  = Servo.MIN_POSITION,
+                    S_PICKUP_START_POS_SL  = Servo.MIN_POSITION,
+                    S_HITCH_START_POS_R    = Servo.MIN_POSITION,
+                    S_HITCH_START_POS_L    = Servo.MIN_POSITION;
 
     // motor powers
-    float   M_drivePowerR = STOP,
+    double  M_drivePowerR = STOP,
             M_drivePowerL = STOP,
             M_pickupPower = STOP,
             M_liftPower   = STOP,
@@ -80,7 +80,7 @@ public class Test extends SynchronousOpMode {
             M_hangPowerL  = STOP;
 
     // servo positions
-    float   S_climbersPosR  = S_CLIMBERS_START_POS_R,
+    double  S_climbersPosR  = S_CLIMBERS_START_POS_R,
             S_climbersPosL  = S_CLIMBERS_START_POS_L,
             S_liftPosR      = S_LIFT_START_POS_R,
             S_liftPosL      = S_LIFT_START_POS_L,
@@ -93,9 +93,18 @@ public class Test extends SynchronousOpMode {
             S_hitchPosR     = S_HITCH_START_POS_R,
             S_hitchPosL     = S_HITCH_START_POS_L;
 
+    double angleTarget, currAngle;
+
     // thread stuff
     ControllerThread R_controllerThread;
     Thread T_controllerThread;
+
+    enum Quadrant {
+        Q1,
+        Q2,
+        Q3,
+        Q4
+    } Quadrant quadrant;
 
 
     @Override public void main() throws InterruptedException {
@@ -136,8 +145,16 @@ public class Test extends SynchronousOpMode {
         parameters.loggingEnabled = false;
         this.adaFruit = ClassFactory.createAdaFruitBNO055IMU(hardwareMap.i2cDevice.get("adaFruit"), parameters);
 
+        // defining threads
         R_controllerThread = new ControllerThread();
         T_controllerThread = new Thread(R_controllerThread, "Controller Tread");
+
+        // fixing motor directions
+        this.M_driveFR.setDirection(DcMotor.Direction.REVERSE);
+        this.M_driveBR.setDirection(DcMotor.Direction.REVERSE);
+        this.M_pickup.setDirection(DcMotor.Direction.REVERSE);
+        this.M_lift.setDirection(DcMotor.Direction.REVERSE);
+        //this.M_hangL.setDirection(DcMotor.Direction.REVERSE);
 
         // Wait for the game to start
         waitForStart();
@@ -178,14 +195,58 @@ public class Test extends SynchronousOpMode {
     }
     private class ControllerThread implements Runnable {
         private final float C_STICK_TOP_THRESHOLD = 0.85f;      // least value for which stick value read from motor will be 1.0f
+        private double joystickAngle, joyStickMagnitude;
 
         // converts all of the controller sticks into more sensitive values
         // use a negative value for y axis since controller reads -1 when pushed forward
-        private float convertStick(float controllerValue) {   return (float) Range.clip(Math.sin(controllerValue * Math.PI / 2 / C_STICK_TOP_THRESHOLD), -1.0d, 1.0d); }
+        private double convertStick(float controllerValue) {   return Range.clip(Math.sin(controllerValue * Math.PI / 2 / C_STICK_TOP_THRESHOLD), -1.0d, 1.0d); }
         // the main loop function
         public void run() {
             try {
                 while(!Thread.currentThread().isInterrupted()) {
+                     //some experimental stuff for finding joystick angles and such
+                    // finds quadrant stick is pointing in
+                    /*if(-gamepad1.left_stick_y >= 0.0f) {
+                        if(gamepad1.left_stick_x >= 0.0f) {
+                            quadrant = Quadrant.Q1;
+                        } else {
+                            quadrant = Quadrant.Q2;
+                        }
+                    } else {
+                        if(gamepad1.left_stick_x > 0.0f) {
+                            quadrant = Quadrant.Q4;
+                        } else {
+                            quadrant = Quadrant.Q3;
+                        }
+                    }
+
+                    // finds angle of stick
+                    switch(quadrant) {
+                        case Q1:
+                            joystickAngle = Math.toDegrees(Math.atan(-gamepad1.left_stick_y / gamepad1.left_stick_x));
+                            joyStickMagnitude = Math.sqrt(Math.pow(gamepad1.left_stick_y, 2) + Math.pow(gamepad1.left_stick_x, 2));
+                            break;
+                        case Q2:
+                            joystickAngle = 180.0d + Math.toDegrees(Math.atan(-gamepad1.left_stick_y / gamepad1.left_stick_x));
+                            joyStickMagnitude = Math.sqrt(Math.pow(gamepad1.left_stick_y, 2) + Math.pow(gamepad1.left_stick_x, 2));
+                            break;
+                        case Q3:
+                            joystickAngle = 180.0d + Math.toDegrees(Math.atan(-gamepad1.left_stick_y / gamepad1.left_stick_x));
+                            joyStickMagnitude = -Math.sqrt(Math.pow(gamepad1.left_stick_y, 2) + Math.pow(gamepad1.left_stick_x, 2));
+                            break;
+                        case Q4:
+                            joystickAngle = 360.0d + Math.toDegrees(Math.atan(-gamepad1.left_stick_y / gamepad1.left_stick_x));
+                            joyStickMagnitude = -Math.sqrt(Math.pow(gamepad1.left_stick_y, 2) + Math.pow(gamepad1.left_stick_x, 2));
+                    }
+                    joyStickMagnitude = Math.sqrt(Math.pow(gamepad1.left_stick_y, 2) + Math.pow(gamepad1.left_stick_x, 2));
+                    if(Math.abs(joystickAngle - adaAngle) > angleThreshold) {
+                        do a pid thing for turning
+                        angleTarget = joystickAngle;
+                    else {
+                        M_drivePowerR = convertStick(joyStickMagnitude);
+                        M_drivePowerL = convertStick(joyStickMagnitude);
+                    }
+                    */
 
                     // motor control block
                     M_drivePowerR = convertStick(-gamepad1.right_stick_y);
@@ -216,6 +277,46 @@ public class Test extends SynchronousOpMode {
                 return;
             }
         }
-
+    }
+    private class TurnPIDThread implements Runnable {
+        final double angleThreshold = 0.5d;
+        float   kP = 0.0f,
+                kI = 0.0f;
+        float   maxPower = 1.0f,
+                minPower = -1.0f,
+                minPIDPower = 0.2f;
+        boolean isMoving = false;
+        double  currError = 0.0d,   // in degrees
+                accumError = 0.0d;  // in degrees
+        float   PIDValue = 0.0f,
+                power = 0.0f;
+        double prevTarget = 0.0d;
+        @Override
+        public void run() {
+            try {
+                while (!Thread.currentThread().isInterrupted()) {
+                    if (Math.abs(prevTarget - angleTarget) > angleThreshold) {
+                        prevTarget = angleTarget;
+                        accumError = 0.0d;
+                    }
+////////////////////////////// find a way to convert degrees to 0-180 from y axis
+                    if (Math.abs(angleTarget - currAngle) > angleThreshold) {
+                        currError = (Math.abs(angleTarget - currAngle) > Math.abs(currAngle - angleTarget)) ? angleTarget - currAngle : currAngle - angleTarget;
+                        accumError += currError;
+                        PIDValue = (float) (kP * currError + kI * accumError);
+                        PIDValue = Range.clip(PIDValue, -maxPower, maxPower);
+                        //driveRPower = PIDValue;
+                        if (Math.abs(currError) < angleThreshold) {
+                            power = 0.0f;
+                            isMoving = false;
+                        }
+                        Thread.sleep(10);
+                    }
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return;
+            }
+        }
     }
 }
